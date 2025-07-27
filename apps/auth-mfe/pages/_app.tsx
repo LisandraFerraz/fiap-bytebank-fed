aec680705196b40d817137410910b953fdaf5179
LisandraFerraz <lisandraferraz.s@gmail.com>
2025-07-27 02:11:52 -0300

    [WIP] configuring global variables pkg

--- apps/auth-mfe/pages/_app.tsx
+++ apps/auth-mfe/pages/_app.tsx
@@ -1,3 +1,4 @@
+import "@bytebank/ui/variables";
 import type { AppProps } from "next/app";
 
 export default function App({ Component, pageProps }: AppProps) {
--- packages/ui/src/index.ts
+++ packages/ui/src/index.ts
@@ -1,5 +1,6 @@
+import "./styles/variables.scss";
+
 export * from "./inputs/input-select/input-select";
 export * from "./inputs/input-text/input-text";
 // export * from "./button/button";
 export * from "./icon/icon";
-export * from "./bytebank-styles/variables.scss";
