d88ac60ddd3b34e99c7555981b09c7d7d04e246d
LisandraFerraz <lisandraferraz.s@gmail.com>
2025-07-31 23:39:39 -0300

    [MOD] adjusting env variables

--- apps/next-bytebank/next.config.ts
+++ apps/next-bytebank/next.config.ts
@@ -26,7 +26,7 @@ const nextConfig: NextConfig = {
     return [
       {
         source: "/auth/:path*",
-        destination: `http://auth-mfe:3002/auth/:path*`,
+        destination: `${process.env.NEXT_PUBLIC_AUTH_MFE_URL}/auth/:path*`,
       },
     ];
   },
